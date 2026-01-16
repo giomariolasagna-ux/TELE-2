818d4fdf101985248901bc5dc5284c908a548b80
Gio Mario Lasagna <giomariolasagna@gmail.com>
2026-01-16 14:26:53 +0100

    Initial Commit

---
+++ TELETests/TELETests.swift
@@ -0,0 +1,16 @@
+//
+//  TELETests.swift
+//  TELETests
+//
+//  Created by Gio Mario Lasagna on 16/01/26.
+//
+
+import Testing
+
+struct TELETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
